9663c359f6c6800d44a7b7a2476a9a3bba565b87
KM <kgmyat@pm.me>
2023-03-15 23:48:33 +0100

    Test GH workflow with TF

---
+++ terraform/terraform.tf
@@ -0,0 +1,27 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+
+  backend "s3" {
+    key = "global/crc-backend/terraform.tfstate"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+# Input variables
+
+variable "aws_region" {
+  description = "Default AWS region for all resources"
+  type        = string
+  default     = "us-east-1"
+}
