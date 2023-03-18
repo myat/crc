afe1b50fa8c14bcfc3127d4a4f1f2a85ce63c0c1
KM <kgmyat@pm.me>
2023-03-18 22:13:03 +0100

    Create variable for visitor count table name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,9 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "dynamodb_table_name" {
+  description = "Name of visitor count DynamoDB table"
+  type        = string
+  default     = "visitor_count_table"
+}
