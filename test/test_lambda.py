5664689c8ce79665023629119c998c0a2de9b9eb
KM <kgmyat@pm.me>
2023-03-19 13:54:09 +0100

    Added fake credentials for mock usage Clean up and comments

--- test/test_lambda.py
+++ test/test_lambda.py
@@ -1,8 +1,10 @@
+# File: test_lambda.py
 import boto3
 import pytest
-from moto import mock_dynamodb
 import sys
 import json
+import os
+from moto import mock_dynamodb
 
 sys.path.append("..")
 
@@ -39,8 +41,17 @@ def update_visitors_event():
     return update_visitors_event
 
 
+@pytest.fixture
+def aws_fake_credentials():
+    """Fake credentials for mock usage."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_KEY'] = 'testing'
+
+
 @pytest.fixture(autouse=True)
-def set_up():
+def set_up(aws_fake_credentials):
     with mock_dynamodb():
         dynamodb = boto3.client('dynamodb', region_name='us-east-1')
         mockTable = dynamodb.create_table(
@@ -74,10 +85,12 @@ def set_up():
                     }
                 }
             )
-        yield dynamodb
+        yield dynamodb, mockTable
         dynamodb = None
 
 
+# Test response to an invalid request
+# Expect a HTTP 400 response
 def test_invalid_request(invalid_event, set_up):
     from src.lambda_function import lambda_handler
     response = lambda_handler(event=invalid_event, context={})
@@ -85,6 +98,8 @@ def test_invalid_request(invalid_event, set_up):
     assert response['statusCode'] == 400
 
 
+# Test response to a getVisitors request
+# Expect a HTTP 200 with non-empty body
 def test_get_visitors(get_visitors_event, set_up):
     from src.lambda_function import lambda_handler
     response = lambda_handler(event=get_visitors_event, context={})
@@ -93,6 +108,8 @@ def test_get_visitors(get_visitors_event, set_up):
     assert response['body'] is not None
 
 
+# Test response to a updateVisitros request
+# Expect a HTTP 200, increased 'views' after successive call
 def test_update_visitors(update_visitors_event, set_up):
     from src.lambda_function import lambda_handler
 
