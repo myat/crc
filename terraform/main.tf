c85c81d82d2ff4b646d77f24cc72f6504e8821f8
KM <kgmyat@pm.me>
2023-03-30 11:49:58 +0200

    Add custom domain config to API GW Changed API GW type to regional

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,15 @@ provider "aws" {
 
   default_tags {
     tags = {
-      stack = "crc-backend"
-      project = "crc"
+      stack       = "crc-backend"
+      project     = "crc"
       environment = "staging"
     }
   }
+}
+
+provider "gandi" {
+  key = data.aws_ssm_parameter.ssm_gandi_api_key.value
 }
 
 # Upload function archive to S3 
@@ -95,6 +99,10 @@ resource "aws_dynamodb_table_item" "visitor_counter_table_bootstrap_item" {
 # Define the use of rest API
 resource "aws_api_gateway_rest_api" "visitor_api" {
   name = "visitor_api"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
 # The resource for the endpoint
@@ -137,7 +145,72 @@ resource "aws_api_gateway_deployment" "lambda_api_deployment" {
   ]
 
   rest_api_id = aws_api_gateway_rest_api.visitor_api.id
-  stage_name  = "test"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Set API stage name
+resource "aws_api_gateway_stage" "lambda_api_deployment_stage" {
+  deployment_id = aws_api_gateway_deployment.lambda_api_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.visitor_api.id
+  stage_name    = var.infra_environment
+}
+
+# Set custom domain name for use with API gateway
+resource "aws_api_gateway_domain_name" "api_gateway_custom_domain" {
+  domain_name              = var.api_gateway_custom_domain
+  regional_certificate_arn = aws_acm_certificate_validation.api_domain_cert_dns_validation.certificate_arn
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+# Issue a certificate in ACM with DNS validation option
+
+resource "aws_acm_certificate" "api_domain_cert" {
+  domain_name       = var.api_gateway_custom_domain
+  validation_method = "DNS"
+}
+
+# Add DNS validation record on Gandi
+
+resource "gandi_livedns_record" "dns_validation_record" {
+  zone = data.gandi_domain.apex_domain_zone.id
+
+  # Gandi DNS records need to be without the apex domain
+  name = replace(tolist(aws_acm_certificate.api_domain_cert.domain_validation_options)[0].resource_record_name, ".${var.apex_domain}.", "")
+
+  type   = tolist(aws_acm_certificate.api_domain_cert.domain_validation_options)[0].resource_record_type
+  ttl    = 300
+  values = [tolist(aws_acm_certificate.api_domain_cert.domain_validation_options)[0].resource_record_value]
+}
+
+# Verify DNS validation records
+
+resource "aws_acm_certificate_validation" "api_domain_cert_dns_validation" {
+  certificate_arn = aws_acm_certificate.api_domain_cert.arn
+  depends_on = [
+    gandi_livedns_record.dns_validation_record,
+  ]
+}
+
+# Map custom domain to API stage
+resource "aws_api_gateway_base_path_mapping" "api_domain_mapping" {
+  api_id      = aws_api_gateway_rest_api.visitor_api.id
+  stage_name  = aws_api_gateway_stage.lambda_api_deployment_stage.stage_name
+  domain_name = aws_api_gateway_domain_name.api_gateway_custom_domain.domain_name
+}
+
+# Add CNAME to API regional domain name on Gandi
+resource "gandi_livedns_record" "cname_api_gateway" {
+  zone   = data.gandi_domain.apex_domain_zone.id
+  name   = replace(var.api_gateway_custom_domain, ".${var.apex_domain}", "")
+  type   = "CNAME"
+  ttl    = 300
+  values = ["${aws_api_gateway_domain_name.api_gateway_custom_domain.regional_domain_name}."]
 }
 
 # Add CORS config
