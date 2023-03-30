1d0937b5dd4f001d1aac7b9a02935d991d85bf33
KM <kgmyat@pm.me>
2023-03-30 11:46:36 +0200

    Added allowed domain lists for CORS

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,16 +1,32 @@
 import json
 import boto3
 
+DYNAMODB_TABLE_NAME = 'visitor_count_table'
+
 GET_RAW_PATH = '/getVisitors'
 UPDATE_RAW_PATH = '/updateVisitors'
 HTTP_VERB_GET = 'GET'
 HTTP_VERB_POST = 'POST'
-DYNAMODB_TABLE_NAME = 'visitor_count_table'
+
+ALLOWED_ORIGINS_LIST = [
+    'https://resume.kgmy.at',
+    'https://staging.kgmy.at',
+    'https://test.kgmy.at']
 
 client = boto3.client('dynamodb', region_name='us-east-1')
 
 
+def get_allow_origin(request_origin):
+    if request_origin in ALLOWED_ORIGINS_LIST:
+        return {'Access-Control-Allow-Origin': request_origin}
+    else:
+        return {'Access-Control-Allow-Origin': ALLOWED_ORIGINS_LIST[0]}
+
+
 def lambda_handler(event, context):
+    origin_header = get_allow_origin(event.get('headers', {}).get('origin'))
+    print("Origin header > ", origin_header)
+
     if (event['path'] == GET_RAW_PATH and
             event['httpMethod'] == HTTP_VERB_GET):
         print("Starting request for getVisitors")
@@ -27,9 +43,9 @@ def lambda_handler(event, context):
             'statusCode': 200,
             'headers': {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Headers': '*',
-                'Access-Control-Allow-Origin': 'https://resume.kgmy.at',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'OPTIONS,GET',
+                **origin_header
                 },
             'body': json.dumps(data['Item'])
         }
@@ -68,9 +84,9 @@ def lambda_handler(event, context):
             'statusCode': 200,
             'headers': {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Headers': '*',
-                'Access-Control-Allow-Origin': 'https://resume.kgmy.at',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'OPTIONS,GET',
+                **origin_header
                 },
             'body': json.dumps(data['Attributes'])
         }
@@ -80,9 +96,9 @@ def lambda_handler(event, context):
             'statusCode': 400,
             'headers': {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Headers': '*',
-                'Access-Control-Allow-Origin': 'https://resume.kgmy.at',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'OPTIONS,GET',
+                **origin_header
                 },
             'body': 'Unsupported'
         }
