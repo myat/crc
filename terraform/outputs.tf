76ba7dbe5f8fdf7e211bff648b5ab85b8e2a1c5b
KM <kgmyat@pm.me>
2023-03-30 11:50:21 +0200

    Export API GW domain outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,3 +11,15 @@ output "api_gw_url" {
 
   value = aws_api_gateway_deployment.lambda_api_deployment.invoke_url
 }
+
+output "api_gw_regional_domain" {
+  description = "Regional domain for API GW"
+
+  value = aws_api_gateway_domain_name.api_gateway_custom_domain.regional_domain_name
+}
+
+output "api_gw_customain_domain" {
+  description = "Custom domain for API GW"
+
+  value = aws_api_gateway_domain_name.api_gateway_custom_domain.domain_name
+}
