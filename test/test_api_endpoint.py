cb87880d788af480b21198fc97de36aa75eb958f
KM <kgmyat@pm.me>
2023-03-31 23:47:31 +0200

    Add CORS header checks

--- test/test_api_endpoint.py
+++ test/test_api_endpoint.py
@@ -2,7 +2,6 @@
 # Test live API endpoint
 # Takes --api-gw-domain input
 import pytest
-import json
 import requests
 
 
@@ -35,23 +34,30 @@ def test_invalid_request(update_visitors_api_url):
 
 # Test live response to a getVisitors request
 # Expect a HTTP 200 with non-empty body
+# Expect a default allowed domain in CORS header
 def test_get_visitors_api_endpoint_response(get_visitors_api_url):
     response = requests.get(get_visitors_api_url)
-    response_json = json.loads(response.text)
-    print(response_json)
 
-    assert response.status_code == 200
     assert response is not None
+    assert response.status_code == 200
+    assert response.headers['Access-Control-Allow-Origin'] == \
+        "https://resume.kgmy.at"
 
 
 # Test live response to a updateVisitros request
 # Expect a HTTP 200, increased 'views' after successive call
+# Expect a default allowed domain in CORS header
 def test_update_visitors_api_endpoint_response(update_visitors_api_url):
     response1 = requests.get(update_visitors_api_url)
     response2 = requests.get(update_visitors_api_url)
 
     assert response1.status_code == 200
+    assert response1.headers['Access-Control-Allow-Origin'] == \
+        "https://resume.kgmy.at"
+
     assert response2.status_code == 200
+    assert response1.headers['Access-Control-Allow-Origin'] == \
+        "https://resume.kgmy.at"
 
     response1_views = response1.json()['views']['N']
     response2_views = response2.json()['views']['N']
