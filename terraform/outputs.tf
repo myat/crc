8fb9cb5cd21ee5f6d075cb4ef8001fd5b1efbe5b
KM <kgmyat@pm.me>
2023-04-03 16:49:52 +0200

    Export cf_alias_domain in TF output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,5 @@
 # Output values
 
-output "frontend_bucket_name" {
-  description = "Name of the frontend files S3 bucket"
-
-  value = aws_s3_bucket.frontend_bucket.id
-}
-
 output "cf_dist_id" {
   description = "ID of the CloudFront distribution"
 
@@ -13,9 +7,15 @@ output "cf_dist_id" {
 }
 
 output "cf_dist_domain_name" {
-  description = "URL of the CloudFront distribution"
+  description = "CloudFront distribution domain"
 
   value = aws_cloudfront_distribution.frontend_distribution.domain_name
+}
+
+output "cf_alias_domain" {
+  description = "CF alias domain"
+
+  value = tolist(aws_cloudfront_distribution.frontend_distribution.aliases)[0]
 }
 
 output "acm_cert_time" {
