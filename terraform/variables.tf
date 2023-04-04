9db53a98b890eef3963a8bb148dbc63dc9c1a9c2
KM <kgmyat@pm.me>
2023-04-04 15:37:04 +0200

    Create input variable static_files_path Set deployment_env to lowercase

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,18 +6,34 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+# Change this variable for PROD deployment
+# Downstream locals: local.default_tags, local.cf_alias_domain
+
 variable "deployment_env" {
   description = "The environment to deploy to. This has impact on domain names among others"
   type        = string
-  default     = "STAGE"
+  default     = "stage"
   nullable    = false
 
   validation {
-    condition     = contains(["STAGE", "PROD"], var.deployment_env)
-    error_message = "Variable deployment_env must be one of: STAGE (default), PROD."
+    condition     = contains(["stage", "prod"], var.deployment_env)
+    error_message = "Variable deployment_env must be one of: stage (default), prod."
   }
 }
 
+# Path to get the site static files from
+# These will be uploaded to S3
+# Could be modified during PROD deployment (e.g. ../dist/)
+
+variable "static_files_path" {
+  description = "Path to static files to be hosted on S3"
+  type        = string
+  default     = "../src"
+  nullable    = false
+}
+
+
+# Domain name to get the Gandi DNS zone
 
 variable "apex_domain" {
   description = "Primary domain name"
