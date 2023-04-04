3ef6ea5642121282a8b6f9f3076461a9179c9658
KM <kgmyat@pm.me>
2023-04-04 17:32:50 +0200

    Cleanup

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,5 @@
 # Output values
 
-output "lambda_bucket_name" {
-  description = "Name of the S3 bucket used to store Lambda code"
-
-  value = aws_s3_bucket.lambda_bucket.id
-}
-
 output "api_gw_url" {
   description = "URL of the API GW endpoint"
 
